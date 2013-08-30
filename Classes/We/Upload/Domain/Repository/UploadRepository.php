e565a5bbfda63abd75546fbca7914e87245b0c1e
Man Math <mathman@essentials37>
2013-08-30 09:02:30 +0700

    add controller, repository and template sample

---
+++ Classes/We/Upload/Domain/Repository/UploadRepository.php
@@ -0,0 +1,20 @@
+<?php
+namespace We\Upload\Domain\Repository;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "We.Upload".             *
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Persistence\Repository;
+
+/**
+ * @Flow\Scope("singleton")
+ */
+class UploadRepository extends Repository {
+
+	// add customized methods here
+
+}
+?>
